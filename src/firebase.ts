1c1b669a97a1d0ffc572c8bb55e8d1f5cd7db723
rachel4w2 <rachel4w2@naver.com>
2023-10-09 14:23:17 +0900

    setup

---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDK5p7RwpbOXJL22yoGB5IBG4eMKBKa5n8",
+  authDomain: "nwitter-reloaded-614e5.firebaseapp.com",
+  projectId: "nwitter-reloaded-614e5",
+  storageBucket: "nwitter-reloaded-614e5.appspot.com",
+  messagingSenderId: "713101106129",
+  appId: "1:713101106129:web:b798d091bd66dc995f5ef0",
+};
+
+const app = initializeApp(firebaseConfig);
